f65f6c64e799759d536a340e2f24f5a68a6b1db6
Tasreen Mowreen <tmowreen97@gmail.com>
2022-12-02 19:07:52 -0500

    commit tree f80f72d95b30396d2d156ad1123e63265b1a83c0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,25 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    #include associated revies in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: {only: [:comment, :score], include: {
+        user: {only: [:name]}
+      }}
+    })
   end
 
 end
